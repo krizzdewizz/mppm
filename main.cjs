00d5791617975a9bdddbcbc89e96f7384afc836d
christian.oetterli <christian.oetterli@bison-group.com>
2022-10-22 13:26:06 +0200

    dark scrollbar in electron

--- main.cjs
+++ main.cjs
@@ -25,6 +25,7 @@ function createWindow() {
     webPreferences: {
       preload: path.join(__dirname, 'preload.cjs'),
       nodeIntegration: true,
+      enableBlinkFeatures: "CSSColorSchemeUARendering",
     },
   });
 
