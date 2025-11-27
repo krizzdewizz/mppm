0505df491186a9053837df918d24fd40a0ba62f0
Christian Oetterli <christian@o3tt3rli.com>
2025-11-27 18:46:10 +0100

    format/cleanup

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mppm-ng',
-  webDir: 'www'
-};
-
-export default config;
